918e4e0e9ccf926316a5a38519bcf3d40e5f4e5b
Gaurav Kumar <gauravkumar15021999@gmail.com>
2025-01-23 16:23:55 +0530

    Add Tech component and update Grid links and statistics

---
+++ src/components/Tech/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const Tech = () => {
+  return (
+    <div className="flex justify-center items-center h-110 py-2">
+      <img
+        src="/images/tech.png" alt="Tech" className="h-[100%]" />
+    </div>
+  );
+};
+
+export default Tech;
